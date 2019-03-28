319472e7b539af8da7e7ebf8a1649fe624172754
Ramesh Ghimire <rameshxghimire@gmail.com>
2019-03-28 15:52:07 +0200

    --CRUD API functions and setting routes

--- app.py
+++ app.py
@@ -48,6 +48,65 @@ item_schema = ItemSchema(strict = True)
 items_schema = ItemSchema(many=True, strict=True)
 
 
+# API function: Create an item
+@app.route("/item", methods=["POST"])
+def add_item():
+    name = request.json["name"]
+    description = request.json["description"]
+    available = request.json["available"]
+    last_ordered = request.json["last_ordered"]
+
+    new_item = Item(name, description, available, last_ordered)
+
+    db.session.add(new_item)
+    db.session.commit()
+
+    return item_schema.jsonify(new_item)
+
+
+# API function: Get all items
+@app.route("/items", methods=["GET"])
+def get_items():
+    all_items = Item.query.all()
+    result = items_schema.dump(all_items)
+    return jsonify(result.data)
+
+
+# API function:: Get individual items by id
+@app.route("/item/<id>", methods=["GET"])
+def get_item_by_id(id):
+    item = Item.query.get(id)
+    return item_schema.jsonify(item)
+
+
+# API function: Update an item
+@app.route("/item/<id>", methods=["PUT"])
+def update_item(id):
+    item = Item.query.get(id)
+    name = request.json["name"]
+    description = request.json["description"]
+    available = request.json["available"]
+    last_ordered = request.json["last_ordered"]
+
+    item.name = name
+    item.description = description
+    item.available = available
+    item.last_ordered = last_ordered
+
+    db.session.commit()
+
+    return item_schema.jsonify(item)
+
+
+# API function: Delete an item
+@app.route("/item/<id>", methods=["DELETE"])
+def delete_item(id):
+    item = Item.query.get(id)
+    db.session.delete(item)
+    db.session.commit()
+
+    return item_schema.jsonify(item)
+
 
 if __name__ == "__main__":
     app.run(debug=True)  # debug set to True for dev env
