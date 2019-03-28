4c0ee2e24a6b3e7310152f0ee17ed596d992b411
Ramesh Ghimire <rameshxghimire@gmail.com>
2019-03-28 15:41:25 +0200

    --Schema setups

--- app.py
+++ app.py
@@ -22,6 +22,33 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
+# Inventory Item Class
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(250))
+    available = db.Column(db.Integer)
+    last_ordered = db.Column(db.Text)
+
+    def __init__(self, name, description, available, last_ordered):
+        self.name = name
+        self.description = description
+        self.available = available
+        self.last_ordered = last_ordered
+
+
+# Create the schema
+class ItemSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "description", "available", "last_ordered")
+
+
+# Initialise the schema
+item_schema = ItemSchema(strict = True)
+items_schema = ItemSchema(many=True, strict=True)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)  # debug set to True for dev env
 
