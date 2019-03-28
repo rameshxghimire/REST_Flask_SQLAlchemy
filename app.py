b9d6561f10eb07eb8ec91c40da031063b6eeb1bd
Ramesh Ghimire <rameshxghimire@gmail.com>
2019-03-28 15:36:27 +0200

    initialisation script

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 
+# Initialise the app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == "__main__":
     app.run(debug=True)  # debug set to True for dev env
