d20dd7bf261d0c275933d3144a4c4ac112c86b0b
Ramesh Ghimire <rameshxghimire@gmail.com>
2019-03-28 15:35:37 +0200

    --added files

---
+++ app.py
@@ -0,0 +1,15 @@
+""" app.py the main application file. """
+
+# imports
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # debug set to True for dev env
+
+# todo: add vendors
+# todo: add authentication
